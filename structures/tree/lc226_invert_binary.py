c3171931aa28b61d33ae62c1b82f117f2cc6585a
Andrius <Andrius.Rumsa@mail.schwarz>
2025-06-11 10:46:20 +0200

    [ADD]: BFS solution

--- structures/tree/lc226_invert_binary.py
+++ structures/tree/lc226_invert_binary.py
@@ -7,6 +7,7 @@ class TreeNode:
 
 from collections import deque
 from typing import Optional
+import copy
 
 
 class Solution:
@@ -21,14 +22,22 @@ class Solution:
         while que:
             curr_node = que.popleft()
             if curr_node is not None:
-                left_node = curr_node.left
-                right_node = curr_node.right
-                if curr_node.left:
-                    curr_node.left = right_node
-                    que.append(right_node)
-                if curr_node.right:
-                    curr_node.right = left_node
-                    que.append(left_node)
+                # left_node = copy.deepcopy(curr_node.left)
+                # right_node = copy.deepcopy(curr_node.right)
+                # Why it works even without deepcopy?
+                left_node = curr_node.left # left_node points to -> curr_node.left
+                right_node = curr_node.right # right_node points to -> curr_node.right
+
+                curr_node.left = right_node # curr_node.left points to -> right_node -> curr_node.right
+                que.append(left_node)
+
+                curr_node.right = left_node # curr_node.right points to -> left_node -> curr_node.left
+                que.append(right_node)
+
+                # So: 1. left -> curr.left
+                # then 2. right -> curr.right
+                # 3. curr.left -> right -> curr.right
+                # left still points to -> curr.left that is why curr.right -> left -> curr.left still works
 
         return root
 
