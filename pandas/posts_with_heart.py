cb611ce0c31cb703d78a3da42b3b782f0abfcfbe
Andrius Rumša <andrius.rumsha@gmail.com>
2026-06-12 07:59:00 +0200

    feat(pandas): merge and filter to find the posts with hearts
    lesson learned: always check for unique values: who told you that a like
    and a heart are the same?! noone. then check, bro

---
+++ pandas/posts_with_heart.py
@@ -0,0 +1,61 @@
+# pyrefly: ignore-errors
+# ruff: noqa
+
+# Import your libraries
+
+# Start writing code
+facebook_reactions.head()
+
+"""
+# Goal
+
+Find all posts with a heart 
+
+# Output
+
+All columns from facebook_posts for the posts reacted with a heart
+
+# Anchor
+
+Facebook_posts table since it is the output
+
+# Filter/Remove
+
+Remove the unnecessary cols
+
+# Join
+
+Join facebook_reactions with facebook_posts
+
+# Operations
+
+1. Join by post id
+
+2. filter by reaction == like
+
+# Assumptions 
+
+all posts have a reaction
+
+
+# Edge Cases
+
+Missing values after join (missaligned tables)
+Nulls, dupes
+"""
+print(facebook_reactions["reaction"].unique())  # like and heart are not the same!!
+
+output_col = list(facebook_posts.columns) + ["reaction"]
+joined_df = facebook_posts.merge(
+    right=facebook_reactions, on=["post_id"], how="left", suffixes=("", "_right")
+)[output_col]
+
+no_nulls_joined_df = joined_df.dropna(subset="reaction")
+print("N reaction nulls: ", len(joined_df) - len(no_nulls_joined_df))
+
+output = (
+    no_nulls_joined_df[no_nulls_joined_df["reaction"] == "heart"]
+    .drop(columns=["reaction"])
+    .drop_duplicates(subset=["post_id"])
+)
+output
